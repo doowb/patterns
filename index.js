01c7845e7f84239ba16ae0947ad7e791c3b27575
Brian Woodward <brian.woodward@gmail.com>
2014-10-23 13:58:55 -0400

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * patterns <https://github.com/doowb/patterns>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
